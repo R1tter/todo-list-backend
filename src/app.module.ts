feeb2c2492e543a45e7efcc3a3d498dd9458420c
r1tter <marcelosori@hotmail.com>
2023-10-18 14:03:47 -0300

    feat: sqlite configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './database.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tasks/tasks.resolver.ts
+++ src/tasks/tasks.resolver.ts
@@ -1,16 +1,43 @@
-import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 import { CreateTaskInput } from './create-task.input';
+import { UpdateTaskInput } from './update-task.input'; // Você precisará criar esse DTO
 
 @Resolver(() => Task)
 export class TasksResolver {
   constructor(private tasksService: TasksService) {}
 
+  // 1. Mostrar a lista de tarefas
+  @Query(() => [Task])
+  async getTasks(): Promise<Task[]> {
+    return this.tasksService.findAll();
+  }
+
+  // 2. Permitir cadastro de nova tarefa
   @Mutation(() => Task)
   async createTask(@Args('input') input: CreateTaskInput): Promise<Task> {
     return this.tasksService.createTask(input);
   }
 
-  // Adicione mais resolvers conforme necessário, como queries para obter tarefas, mutações para atualizar ou deletar tarefas, etc.
+  // 3. Marcar a tarefa como concluída
+  @Mutation(() => Task)
+  async markTaskAsCompleted(@Args('id') id: number): Promise<Task> {
+    return this.tasksService.markAsCompleted(id);
+  }
+
+  // 4. Editar tarefa
+  @Mutation(() => Task)
+  async updateTask(
+    @Args('id') id: number,
+    @Args('input') input: UpdateTaskInput,
+  ): Promise<Task> {
+    return this.tasksService.updateTask(id, input);
+  }
+
+  // 5. Deletar tarefa
+  @Mutation(() => Boolean)
+  async deleteTask(@Args('id') id: number): Promise<boolean> {
+    return this.tasksService.deleteTask(id);
+  }
 }
